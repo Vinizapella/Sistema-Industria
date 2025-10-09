f677dfa7076d9a781d6e33122e9c8ec057d27b74
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2025-10-09 19:47:23 -0300

    Funcionalidade cadastrar maquina concluida

--- src/main/java/org/example/Conexao.java
+++ src/main/java/org/example/Conexao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = "jdbc:mysql://localhost:3306/industria?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/logistica?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String SENHA = "root";
+    private static final String SENHA = "mysqlPW";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
